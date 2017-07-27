f98557e7c39f56508cad181d7a47616ea14a4475
Jacek Całusiński <forger@forger.pl>
2017-07-27 22:40:53 +0200

    Add timestamp range pairs generator
    This commit introduces helpers for generating random timestamp ranges.
    It can be used for e.g. generating multiple non-overlapping partitions.

--- hyena-engine/src/helpers/random.rs
+++ hyena-engine/src/helpers/random.rs
@@ -1,8 +1,8 @@
 pub(crate) mod timestamp {
-    use rand::{Rng, thread_rng};
+    use rand::{thread_rng, Rng};
     use ty::timestamp::{Timestamp, ToTimestampMicros};
     use chrono::prelude::*;
-    use chrono::naive::{MIN_DATE, MAX_DATE};
+    use chrono::naive::{MAX_DATE, MIN_DATE};
     use std::iter::repeat;
 
     // these should be moved associated consts stabilize
@@ -11,7 +11,7 @@ pub(crate) mod timestamp {
     /// minimal UNIX timestamp, used in the random generator
     const TS_MIN: u64 = 1_u64;
     // arbitrary upper bound, maximal UNIX timestamp, used in the random generator
-    const TS_MAX: u64 = 2_147_472_000_000_u64;
+    const TS_MAX: u64 = 2_147_472_000_000_000_u64;
 
     pub(crate) trait RandomTimestamp {
         fn random<T>() -> T
@@ -20,6 +20,16 @@ pub(crate) mod timestamp {
             Timestamp: From<T>,
         {
             Self::random_range::<T, u64>(TS_MIN, TS_MAX)
+        }
+
+        fn random_from<T, TS>(from: TS) -> T
+        where
+            T: From<Timestamp>,
+            Timestamp: From<T> + From<TS>,
+        {
+            let ts = Timestamp::from(from);
+
+            Self::random_range::<T, u64>(*ts, TS_MAX)
         }
 
         fn random_range<T, TS>(from: TS, to: TS) -> T
@@ -101,6 +111,61 @@ pub(crate) mod timestamp {
                 Self::random_range_rng(&mut rng.clone(), from.clone(), to.clone())
             }))
         }
+
+        pub(crate) fn pairs<T>(count: usize) -> Vec<(T, T)>
+        where
+            T: From<Timestamp> + Ord + Clone,
+            Timestamp: From<T>,
+        {
+            Self::range_pairs::<T, u64>(TS_MIN, TS_MAX, count)
+        }
+
+        pub(crate) fn pairs_rng<T, R>(rng: R, count: usize) -> Vec<(T, T)>
+        where
+            T: From<Timestamp> + Ord + Clone,
+            Timestamp: From<T>,
+            R: Rng + Sized + Clone + 'static,
+        {
+            Self::range_pairs_rng::<T, u64, _>(rng, TS_MIN, TS_MAX, count)
+        }
+
+        pub(crate) fn range_pairs<T, TS>(from: TS, to: TS, count: usize) -> Vec<(T, T)>
+        where
+            T: From<Timestamp> + Ord + Clone,
+            Timestamp: From<T> + From<TS>,
+            TS: Clone + 'static,
+        {
+            Self::range_pairs_rng(thread_rng(), from, to, count)
+        }
+
+        pub(crate) fn range_pairs_rng<T, TS, R>(
+            rng: R,
+            from: TS,
+            to: TS,
+            count: usize,
+        ) -> Vec<(T, T)>
+        where
+            T: From<Timestamp> + Ord + Clone,
+            Timestamp: From<T> + From<TS>,
+            TS: Clone + 'static,
+            R: Rng + Sized + Clone + 'static,
+        {
+            let from = Timestamp::from(from);
+            let to = Timestamp::from(to);
+
+            let mut tsvec = repeat(RandomTimestampGen)
+                .take(count * 2)
+                .map(|_| Self::random_range_rng(&mut rng.clone(), from, to))
+                .collect::<Vec<T>>();
+
+            tsvec.sort();
+
+            tsvec
+                .as_slice()
+                .chunks(2)
+                .map(|c| (c[0].clone(), c[1].clone()))
+                .collect()
+        }
     }
 
     impl RandomTimestamp for RandomTimestampGen {}
@@ -129,6 +194,23 @@ pub(crate) mod timestamp {
         #[test]
         fn ts_naive_datetime() {
             let ts = Timestamp::random::<NaiveDateTime>();
+        }
+
+        #[test]
+        fn ts_from_timestamp() {
+            let lo = 140_000_000_u64;
+
+            let ts: u64 = Timestamp::random_from(lo);
+
+            assert!(ts > lo);
+        }
+
+        #[test]
+        fn ts_from_datetime() {
+            let lo = NaiveDate::from_ymd(2016, 04, 01).and_hms(12, 40, 00);
+            let ts: NaiveDateTime = Timestamp::random_from(lo);
+
+            assert!(ts > lo);
         }
 
         #[test]
@@ -173,6 +255,84 @@ pub(crate) mod timestamp {
                 .count();
 
             assert_eq!(count, len);
+        }
+
+        #[test]
+        fn pairs_u64() {
+            let count = 100;
+
+            let p = RandomTimestampGen::pairs::<u64>(count);
+
+            assert_eq!(p.len(), count);
+
+            let mut plo = &(p[0].0);
+
+            for r in &p {
+                let lo = &r.0;
+                let hi = &r.1;
+                assert!(lo <= hi);
+                assert!(hi >= plo);
+                plo = &hi;
+            }
+        }
+
+        #[test]
+        fn pairs_datetime() {
+            let count = 100;
+
+            let p = RandomTimestampGen::pairs::<NaiveDateTime>(count);
+
+            assert_eq!(p.len(), count);
+
+            let mut plo = &(p[0].0);
+
+            for r in &p {
+                let lo = &r.0;
+                let hi = &r.1;
+                assert!(lo <= hi);
+                assert!(hi >= plo);
+                plo = &hi;
+            }
+        }
+
+        #[test]
+        fn pairs_range_u64() {
+            let rlo = 140_000_000_u64;
+            let rhi = 150_000_000_u64;
+            let count = 100;
+
+            let p = RandomTimestampGen::range_pairs::<u64, _>(rlo, rhi, count);
+
+            assert_eq!(p.len(), count);
+
+            let mut plo = &(p[0].0);
+
+            for &(ref lo, ref hi) in &p {
+                assert!(lo <= hi);
+                assert!(hi >= plo);
+                assert!(lo >= &rlo && hi <= &rhi);
+                plo = &hi;
+            }
+        }
+
+        #[test]
+        fn pairs_range_datetime() {
+            let rlo = NaiveDate::from_ymd(2016, 04, 01).and_hms(12, 40, 00);
+            let rhi = NaiveDate::from_ymd(2017, 08, 12).and_hms(15, 40, 00);
+            let count = 100;
+
+            let p = RandomTimestampGen::range_pairs::<NaiveDateTime, _>(rlo, rhi, count);
+
+            assert_eq!(p.len(), count);
+
+            let mut plo = &(p[0].0);
+
+            for &(ref lo, ref hi) in &p {
+                assert!(lo <= hi);
+                assert!(hi >= plo);
+                assert!(lo >= &rlo && hi <= &rhi);
+                plo = &hi;
+            }
         }
     }
 }
