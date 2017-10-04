43aa1fa0cd9c2f45e9ae9a6c779a4fe1c9cd7f30
Tomasz Barański <tomasz.baranski@collective-sense.com>
2017-10-04 14:32:18 +0200

    Set up logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate flexi_logger;
+extern crate colored_logger;
+#[macro_use]
+extern crate log;
+extern crate dotenv;
+
+use dotenv::dotenv;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    flexi_logger::Logger::with_env()
+        .format(colored_logger::formatter)
+        .start()
+        .unwrap_or_else(|e| panic!("Logger initialization failed with {}", e));
+
+    info!("Starting Hyena");
 }
