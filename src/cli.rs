889c80a791a34b1be565068ea011f73d4dff12c8
Tomasz Barański <tomasz.baranski@collective-sense.com>
2017-10-24 11:47:07 +0200

    Testing `validate_port`

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,15 @@
 use clap::{App, Arg};
 
+static PORT_NUMBER_ERROR_STRING: &'static str = "Port number must be between 1 and 65535";
+
 fn validate_port(port_string: String) -> Result<(), String> {
     match port_string.parse::<u16>() {
         Ok(val) => if val == 0 {
-            Err("Port number must be between 1 and 65535".into())
+            Err(PORT_NUMBER_ERROR_STRING.into())
         } else {
             Ok(())
         },
-        Err(_) => return Err("Port number must be between 1 and 65535".into())
+        Err(_) => return Err(PORT_NUMBER_ERROR_STRING.into())
     }
 }
 
@@ -50,4 +52,55 @@ pub fn app() -> App<'static, 'static> {
              .default_value("/tmp/hyena-ipc")
              .short("i")
              .long("ipc-path"))
+}
+
+#[cfg(test)]
+mod tests {
+
+    mod validate_port {
+        use cli::validate_port;
+
+        #[test]
+        fn rejects_empty_string() {
+            assert!(validate_port("".into()).is_err())
+        }
+
+        #[test]
+        fn rejects_non_numbers() {
+            assert!(validate_port("test".into()).is_err());
+            assert!(validate_port("----".into()).is_err());
+            assert!(validate_port("  ".into()).is_err());
+            assert!(validate_port("@#$%".into()).is_err());
+            assert!(validate_port("99beers_on_the_wall".into()).is_err());
+        }
+
+        #[test]
+        fn rejects_negative_numbers() {
+            assert!(validate_port("-4321".into()).is_err());
+            assert!(validate_port("-0".into()).is_err());
+        }
+
+        #[test]
+        fn rejects_big_numbers() {
+            assert!(validate_port("655350".into()).is_err());
+            assert!(validate_port("65536".into()).is_err());
+        }
+
+        #[test]
+        fn rejects_float_numbers() {
+            assert!(validate_port("4321.1".into()).is_err());
+        }
+
+        #[test]
+        fn rejects_zero() {
+            assert!(validate_port("0".into()).is_err());
+        }
+
+        #[test]
+        fn allows_valid_port_numbers() {
+            assert!(validate_port("65535".into()).is_ok());
+            assert!(validate_port("1".into()).is_ok());
+            assert!(validate_port("4321".into()).is_ok());
+        }
+    }
 }
