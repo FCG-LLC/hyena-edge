f19f89dc88a16cd9477521d3384e2dd9b263f402
Jacek Całusiński <forger@forger.pl>
2018-05-23 17:32:15 +0200

    Support HYENA_DEBUG env
    Set HYENA_DEBUG=1 to enable a predefined debug logging profile.

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,39 @@ extern crate bincode;
 extern crate nanomsg_multi_server;
 
 use dotenv::dotenv;
+use std::env::{var_os, set_var};
 
 mod cli;
 mod nanoserver;
 
-fn main() {
+fn configure_logging_env() {
+    let hyena_debug_env = "HYENA_DEBUG";
+    let rust_log_env = "RUST_LOG";
+    let default_logging_params = "info";
+    let default_debug_logging_params = "hyena=debug,hyena_engine=debug,hyena_api=debug,\
+        hyena_common=debug,nanomsg_multi_server=debug";
+
+    let set_rust_log = |value: &str| {
+        set_var(rust_log_env, value);
+    };
+
     dotenv().ok();
+
+    // RUST_LOG overwrites everything
+    if var_os(&rust_log_env).is_none() {
+        if let Some(debug) = var_os(hyena_debug_env) {
+            set_rust_log(match debug.to_str() {
+                Some("1") => default_debug_logging_params,
+                _ => default_logging_params,
+            })
+        } else {
+            set_rust_log(default_logging_params);
+        }
+    }
+}
+
+fn main() {
+    configure_logging_env();
 
     let options = cli::app().get_matches();
 
