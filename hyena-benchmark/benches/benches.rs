3c0937ea170a5a8eb1d560dcaf702e4688adf682
Jacek Całusiński <forger@forger.pl>
2018-07-09 15:42:55 +0200

    Introduce parametrized string criterion benches

--- hyena-benchmark/benches/benches.rs
+++ hyena-benchmark/benches/benches.rs
@@ -3,9 +3,10 @@ extern crate criterion;
 extern crate failure;
 extern crate hyena_engine;
 extern crate hyena_test;
+#[macro_use]
 extern crate hyena_common;
 
 mod config;
 mod scan;
 
-criterion_main!(scan::benches);
+criterion_main!(scan::numeric_benches, scan::string_benches);
